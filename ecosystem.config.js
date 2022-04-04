07197acc31e940c8822380950a632da2e6902e0f
odgwuval1 <valentine.madu@outsourceglobal.com>
2022-04-04 12:43:12 +0100

    modify script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@
       script: 'dist/server.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
+      instances: 1, // pm2 instance count
       autorestart: true, // auto restart if process crash
       watch: false, // files change automatic restart
       ignore_watch: ['node_modules', 'logs'], // ignore files change
@@ -24,25 +24,25 @@
         NODE_ENV: 'production',
       },
     },
-    {
-      name: 'dev', // pm2 start App name
-      script: 'ts-node', // ts-node
-      args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
-      exec_mode: 'cluster', // 'cluster' or 'fork'
-      instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
-      merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
-        PORT: 3000,
-        NODE_ENV: 'development',
-      },
-    },
+    // {
+    //   name: 'dev', // pm2 start App name
+    //   script: 'ts-node', // ts-node
+    //   args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
+    //   exec_mode: 'cluster', // 'cluster' or 'fork'
+    //   instance_var: 'INSTANCE_ID', // instance variable
+    //   instances: 2, // pm2 instance count
+    //   autorestart: true, // auto restart if process crash
+    //   watch: false, // files change automatic restart
+    //   ignore_watch: ['node_modules', 'logs'], // ignore files change
+    //   max_memory_restart: '1G', // restart if process use more than 1G memory
+    //   merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
+    //   output: './logs/access.log', // pm2 log file
+    //   error: './logs/error.log', // pm2 error log file
+    //   env: { // environment variable
+    //     PORT: 3000,
+    //     NODE_ENV: 'development',
+    //   },
+    // },
   ],
   deploy: {
     production: {
