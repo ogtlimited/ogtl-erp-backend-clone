270b5b4a29ec32c2c9755336336b102f20af8829
OGTL Devs <65605454+ogtlimited@users.noreply.github.com>
2022-07-14 15:49:03 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,31 @@
  */
  module.exports = {
   apps: [
+//     {
+//       name: 'prod', // pm2 start App name
+//       script: 'dist/server.js',
+//       exec_mode: 'cluster', // 'cluster' or 'fork'
+//       instance_var: 'INSTANCE_ID', // instance variable
+//       instances: 1, // pm2 instance count
+//       autorestart: true, // auto restart if process crash
+//       watch: false, // files change automatic restart
+//       ignore_watch: ['node_modules', 'logs'], // ignore files change
+//       max_memory_restart: '1G', // restart if process use more than 1G memory
+//       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
+//       output: './logs/access.log', // pm2 log file
+//       error: './logs/error.log', // pm2 error log file
+//       env: { // environment variable
+//         PORT: 3000,
+//         NODE_ENV: 'production',
+//       },
+    },
     {
-      name: 'prod', // pm2 start App name
-      script: 'dist/server.js',
+      name: 'dev', // pm2 start App name
+      script: 'ts-node', // ts-node
+      args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
-      instances: 1, // pm2 instance count
+      instances: 2, // pm2 instance count
       autorestart: true, // auto restart if process crash
       watch: false, // files change automatic restart
       ignore_watch: ['node_modules', 'logs'], // ignore files change
@@ -21,28 +40,9 @@
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
         PORT: 3000,
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
       },
     },
-    // {
-    //   name: 'dev', // pm2 start App name
-    //   script: 'ts-node', // ts-node
-    //   args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
-    //   exec_mode: 'cluster', // 'cluster' or 'fork'
-    //   instance_var: 'INSTANCE_ID', // instance variable
-    //   instances: 2, // pm2 instance count
-    //   autorestart: true, // auto restart if process crash
-    //   watch: false, // files change automatic restart
-    //   ignore_watch: ['node_modules', 'logs'], // ignore files change
-    //   max_memory_restart: '1G', // restart if process use more than 1G memory
-    //   merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-    //   output: './logs/access.log', // pm2 log file
-    //   error: './logs/error.log', // pm2 error log file
-    //   env: { // environment variable
-    //     PORT: 3000,
-    //     NODE_ENV: 'development',
-    //   },
-    // },
   ],
   deploy: {
     production: {
